bb84e05ad2a9d37a27185b5c8d1c9c84a0af4161
rakzhodekams <i00nsu@cryptolab.org>
2020-08-21 18:57:42 +0100

    changing values of game

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,17 @@
-const ATTACK_VALUE = 10;
-const STRONG_ATTACK_VALUE = 17;
-const MONSTER_ATTACK_VALUE = 14;
+const ATTACK_VALUE = 2;
+const STRONG_ATTACK_VALUE = 3;
+const MONSTER_ATTACK_VALUE = 44;
 const HEAL_VALUE = 20;
 
-const MODE_ATTACK = 'ATTACK'; // MODE_ATTACK = 0
-const MODE_STRONG_ATTACK = 'STRONG_ATTACK'; // MODE_STRONG_ATTACK = 1
-const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
-const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK';
-const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
-const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
-const LOG_EVENT_GAME_OVER = 'GAME_OVER';
+const MODE_ATTACK = "ATTACK"; // MODE_ATTACK = 0
+const MODE_STRONG_ATTACK = "STRONG_ATTACK"; // MODE_STRONG_ATTACK = 1
+const LOG_EVENT_PLAYER_ATTACK = "PLAYER_ATTACK";
+const LOG_EVENT_PLAYER_STRONG_ATTACK = "PLAYER_STRONG_ATTACK";
+const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
+const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
+const LOG_EVENT_GAME_OVER = "GAME_OVER";
 
-const enteredValue = prompt('Maximum life for you and the monster.', '100');
+const enteredValue = prompt("Maximum life for you and the monster.", "100");
 
 let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
@@ -32,37 +32,37 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
     event: ev,
     value: val,
     finalMonsterHealth: monsterHealth,
-    finalPlayerHealth: playerHealth
+    finalPlayerHealth: playerHealth,
   };
   switch (ev) {
     case LOG_EVENT_PLAYER_ATTACK:
-      logEntry.target = 'MONSTER';
+      logEntry.target = "MONSTER";
       break;
     case LOG_EVENT_PLAYER_STRONG_ATTACK:
       logEntry = {
         event: ev,
         value: val,
-        target: 'MONSTER',
+        target: "MONSTER",
         finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
+        finalPlayerHealth: playerHealth,
       };
       break;
     case LOG_EVENT_MONSTER_ATTACK:
       logEntry = {
         event: ev,
         value: val,
-        target: 'PLAYER',
+        target: "PLAYER",
         finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
+        finalPlayerHealth: playerHealth,
       };
       break;
     case LOG_EVENT_PLAYER_HEAL:
       logEntry = {
         event: ev,
         value: val,
-        target: 'PLAYER',
+        target: "PLAYER",
         finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
+        finalPlayerHealth: playerHealth,
       };
       break;
     case LOG_EVENT_GAME_OVER:
@@ -70,7 +70,7 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
         event: ev,
         value: val,
         finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
+        finalPlayerHealth: playerHealth,
       };
       break;
     default:
@@ -135,30 +135,30 @@ function endRound() {
     removeBonusLife();
     currentPlayerHealth = initialPlayerHealth;
     setPlayerHealth(initialPlayerHealth);
-    alert('You would be dead but the bonus life saved you!');
+    alert("You would be dead but the bonus life saved you!");
   }
 
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert('You won!');
+    alert("You won!");
     writeToLog(
       LOG_EVENT_GAME_OVER,
-      'PLAYER WON',
+      "PLAYER WON",
       currentMonsterHealth,
       currentPlayerHealth
     );
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert('You lost!');
+    alert("You lost!");
     writeToLog(
       LOG_EVENT_GAME_OVER,
-      'MONSTER WON',
+      "MONSTER WON",
       currentMonsterHealth,
       currentPlayerHealth
     );
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-    alert('You have a draw!');
+    alert("You have a draw!");
     writeToLog(
       LOG_EVENT_GAME_OVER,
-      'A DRAW',
+      "A DRAW",
       currentMonsterHealth,
       currentPlayerHealth
     );
@@ -217,7 +217,7 @@ function healPlayerHandler() {
 
 function printLogHandler() {
   for (let i = 0; i < 3; i++) {
-    console.log('------------');
+    console.log("------------");
   }
   let j = 3;
   do {
@@ -245,7 +245,7 @@ function printLogHandler() {
   }
 }
 
-attackBtn.addEventListener('click', attackHandler);
-strongAttackBtn.addEventListener('click', strongAttackHandler);
-healBtn.addEventListener('click', healPlayerHandler);
-logBtn.addEventListener('click', printLogHandler);
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
+logBtn.addEventListener("click", printLogHandler);
